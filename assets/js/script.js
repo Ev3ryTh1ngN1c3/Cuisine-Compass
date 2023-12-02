ea17eed5c70ba95e585e76776d9d1f0a042f05b5
Tina Humphrey <tinahumphrey@Tinas-MacBook-Air.local>
2023-12-02 01:23:43 -0500

    commit javascriptchanges and html changes to project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 var EdmamamUrl = "https://api.edamam.com/api/recipes/v2";
-var PinterestUrl =" https://pin.it/1JyKAWz";
+// var PinterestUrl =" https://pin.it/1JyKAWz";
 
-var EdmamamAPIkey = "7c47c1f19353798865f2d5450e4f4c1c";
-var EdmamamAPIid = "5fe7465b";
+var EdmamamAPIkey = "7c47c1f19353798865f2d5450e4f4c1c"; // API key for Edmamam
+var EdmamamAPIid = "5fe7465b"; // API id for Edmamam
 
-var PinterestAPIkey = "2bea213985msh50cb50abbd7c5aep110f09jsn698b428cce81";
+//var PinterestAPIkey = "2bea213985msh50cb50abbd7c5aep110f09jsn698b428cce81";
 
 var testQuery = "macaroni";
 
@@ -20,56 +20,58 @@ fetch (EdmamamUrl, {
     console.log(data)
 });
 
-var PinterestUrl = 'https://pinterest-downloader-download-pinterest-image-video-and-reels.p.rapidapi.com/api/data?url=https%3A%2F%2Fpin.it%2F5TZnIsk';
-var PinterestOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '2bea213985msh50cb50abbd7c5aep110f09jsn698b428cce81',
-		'X-RapidAPI-Host': 'pinterest-downloader-download-pinterest-image-video-and-reels.p.rapidapi.com'
-	}
-};
-	fetch(PinterestUrl, PinterestOptions)
-    .then(function(response){
-    return response.json();
+//var PinterestUrl = 'https://pinterest-downloader-download-pinterest-image-video-and-reels.p.rapidapi.com/api/data?url=https%3A%2F%2Fpin.it%2F5TZnIsk';
+// var PinterestOptions = {
+	 //method: 'GET',
+	// headers: {
+		// 'X-RapidAPI-Key': '2bea213985msh50cb50abbd7c5aep110f09jsn698b428cce81',
+		// 'X-RapidAPI-Host': 'pinterest-downloader-download-pinterest-image-video-and-reels.p.rapidapi.com'
+	//}
+//};
+//	fetch(PinterestUrl, PinterestOptions)
+  //  .then(function(response){
+    // return response.json();
 
-    }).then(function(data){
-        console.log(data)
-    });
+//    }).then(function(data){
+       // console.log(data)
+  //  });
 
 
-var KeyWordsUrl = 'https://pinterest-downloader-download-pinterest-image-video-and-reels.p.rapidapi.com/api/basesearch?query=messi';
-var KeyWordOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '2bea213985msh50cb50abbd7c5aep110f09jsn698b428cce81',
-		'X-RapidAPI-Host': 'pinterest-downloader-download-pinterest-image-video-and-reels.p.rapidapi.com'
-	}
-};
+// var KeyWordsUrl = 'https://pinterest-downloader-download-pinterest-image-video-and-reels.p.rapidapi.com/api/basesearch?query=messi';
+// var KeyWordOptions = {
+	//method: 'GET',
+	//headers: {
+	//	'X-RapidAPI-Key': '2bea213985msh50cb50abbd7c5aep110f09jsn698b428cce81',
+	//	'X-RapidAPI-Host': 'pinterest-downloader-download-pinterest-image-video-and-reels.p.rapidapi.com'
+	//}
+//};
 
-	fetch(KeyWordsUrl, KeyWordOptions)
-    .then(function(response){
-    return response.json();
+	//fetch(KeyWordsUrl, KeyWordOptions)
+    //.then(function(response){
+    //return response.json();
     
-    }).then(function(data){
-        console.log(data)
-    });
+//  }).then(function(data){
+     //   console.log(data)
+   // });
+const recipeInput = document.querySelector('.recipe-input');
+const searchButton = document.querySelector('.search-button');
 
-var searchFormEl = document.querySelector('#search-form');
+const getRecipe = () => {
+    const recipeName = recipeInput.value.trim(); // Get user entered recipe and remove extra spaces
+    
+    if (!recipeName) return;  // Return if recipeName is empty
+    const EdmamamAPIUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=" + recipeName + "&app_id=" + EdmamamAPIid + "&app_key=" + EdmamamAPIkey;
 
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
-
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
-
-  var queryString = './results.html?=' + searchInputVal + '&format=' + formatInputVal;
-
-  location.assign(queryString);
+    //Get entered recipe from API response
+    fetch(EdmamamAPIUrl).then(res => res.json()).then(data =>{
+        if(!data.length) return alert("No recipe found for " + recipeName);
+        const {  } = data[0];
+    }).catch(() => {
+        alert("An error occurred while fetching the recipe!");
+    });     
 }
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+searchButton.addEventListener("click", getRecipe); 
+
+
+
